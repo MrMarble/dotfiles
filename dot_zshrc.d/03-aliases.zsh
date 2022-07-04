ba39b23b5b8dfc9b27ca5ac1cb186ccd845d2892
Alvaro Tinoco Marmol <alvarotinocomarmol@gmail.com>
2022-07-04 15:24:06 +0200

    add some aliases

--- dot_zshrc.d/03-aliases.zsh
+++ dot_zshrc.d/03-aliases.zsh
@@ -1,1 +1,15 @@
 alias k=kubectl
+alias grep=rg -p
+alias cat=bat -P
+alias vim=nvim
+alias vi=nvim
+
+# Always wear a condom
+alias chmod='chmod --preserve-root -v'
+alias chown='chown --preserve-root -v'
+
+# not aliasing rm -i, but if safe-rm is available, use condom.
+# if safe-rmdir is available, the OS is suse which has its own terrible 'safe-rm' which is not what we want
+if (( ${+commands[safe-rm]} && ! ${+commands[safe-rmdir]} )); then
+  alias rm='safe-rm'
+fi
